f740aea215efe77ed844b62b187403356c39a4bb
Callum Cheshire <84125856+callum-cheshire@users.noreply.github.com>
2024-01-02 17:23:13 +0000

    adjusted max width to fit on desktop screen size

--- nextjs-frontend-mentor_blog-card-preview/src/app/page.js
+++ nextjs-frontend-mentor_blog-card-preview/src/app/page.js
@@ -5,7 +5,7 @@ import avatar from '../assets/images/image-avatar.jpg';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen align-middle p-2'>
-      <div className='card bg-neutral-white text-neutral-black w-11/12 h-[450px] rounded-2xl border-solid border shadow-card overflow-auto'>
+      <div className='card bg-neutral-white text-neutral-black max-w-[300px] w-11/12 h-[450px] rounded-2xl border-solid border shadow-card overflow-auto'>
         <div className='content p-5 h-full flex flex-col justify-between'>
           <Image 
             src={mainGraphic}
@@ -24,7 +24,7 @@ export default function Home() {
 
           <p className='text-neutral-grey text-xs pb-2'>These languages are the backbone of every website, defining structure, content, and presentation.</p>
 
-          <div className='flex items-center pt-2 pb-4'>
+          <div className='flex items-center pt-2'>
             <Image src={avatar} width={30} height={30} alt="Callum Cheshire" />
             <p className='pl-4 font-extrabold text-sm'>Callum Cheshire</p>
           </div>
