34d4b9ad5354d3a0be50cb3692a7474cb62220a8
Callum Cheshire <84125856+callum-cheshire@users.noreply.github.com>
2024-01-02 17:18:52 +0000

    finishing touches to mobile layout

--- nextjs-frontend-mentor_blog-card-preview/src/app/page.js
+++ nextjs-frontend-mentor_blog-card-preview/src/app/page.js
@@ -5,7 +5,7 @@ import avatar from '../assets/images/image-avatar.jpg';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen align-middle p-2'>
-      <div className='card bg-neutral-white text-neutral-black w-11/12 h-[450px] rounded-2xl border-solid border shadow-card overflow-hidden'>
+      <div className='card bg-neutral-white text-neutral-black w-11/12 h-[450px] rounded-2xl border-solid border shadow-card overflow-auto'>
         <div className='content p-5 h-full flex flex-col justify-between'>
           <Image 
             src={mainGraphic}
@@ -15,7 +15,7 @@ export default function Home() {
             className='w-full height-auto rounded-lg'
           />
 
-          <div className='flex flex-col justify-end pt-4 pb-3 overflow-hidden'>
+          <div className='flex flex-col justify-end pt-4 pb-3'>
             <button className='bg-primary-yellow w-1/3 h-1/2 p-2 flex items-center justify-center font-black text-sm rounded-md overflow-hidden'>Learning</button>
             <p className='pt-3 text-xs'>Published 21 Dec 2023</p>
           </div>
@@ -24,7 +24,7 @@ export default function Home() {
 
           <p className='text-neutral-grey text-xs pb-2'>These languages are the backbone of every website, defining structure, content, and presentation.</p>
 
-          <div className='flex items-center pt-2'>
+          <div className='flex items-center pt-2 pb-4'>
             <Image src={avatar} width={30} height={30} alt="Callum Cheshire" />
             <p className='pl-4 font-extrabold text-sm'>Callum Cheshire</p>
           </div>
